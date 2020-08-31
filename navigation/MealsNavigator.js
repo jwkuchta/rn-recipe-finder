885757f0ee5c66d1bf83094ad14e08cfd6883c64
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-31 13:39:03 -0400

    added and configured tab navigation between meals/favorites

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,14 @@
+import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
 import colors from '../constants/colors'
+import { AntDesign } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons'
 
 // configure the initial order of screens (the main screens)
 const MealsNavigator = createStackNavigator({
@@ -23,5 +28,27 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator({
+    // what screens will be displayed when you click on a particular tab
+    Meals: { 
+        screen: MealsNavigator, 
+        navigationOptions: {
+            tabBarIcon: (tabinfo) => <Ionicons name='ios-restaurant' size={25} color={tabinfo.tintColor} />
+    }}, // we can use the MealsNavigator to be loaded when this tab is clicked!
+    Favorites: {
+        screen: FavoritesScreen,
+        navigationOptions: {
+            tabBarIcon: (tabinfo) => <Ionicons name='ios-star' size={25} color={tabinfo.tintColor} />
+        }
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: colors.accent,
+
+    }
+})
+
+// MealsFavTabNavigator now became our root navigator so the below export needs to change
+// export default createAppContainer(MealsNavigator)
+export default createAppContainer(MealsFavTabNavigator)
 
