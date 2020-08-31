9c7000e1f0423ebb194a1893005fb2b69c45e99a
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-31 14:53:50 -0400

    tab navigation complete and working with stack navigation

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,24 +1,20 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import MealList from '../components/MealList'
+import { MEALS } from '../data/dummy-data'
+
 
 const FavoritesScreen = props => {
+
+    // we are here faking the data for favorite meals
+    const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2')
+
     return (
-        <View style={styles.screen}>
-            <Text>FAVORITES SCREEN</Text>
-        </View>
+        <MealList listData={favMeals} navigation={props.navigation} />
     )
 }
 
 FavoritesScreen.navigationOptions = {
     headerTitle: 'Your Favorites'
 }
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
 
 export default FavoritesScreen
