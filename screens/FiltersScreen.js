56f0da3e6429a8f86e56d907e93d246489013b0c
Justina Kuchta <justyna.kuchta@gmail.com>
2020-09-01 13:35:19 -0400

    added filters to the filter screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Switch } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../components/HeaderButton'
+// you need to manually manage state for the Switch
+import colors from '../constants/colors'
+
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.label}</Text>
+            <Switch 
+            value={props.value}
+            // onValueChange={() => setIsGF(!isGF)} //my way
+            trackColor={{true: colors.accent}}
+            // thumbColor={colors.primary}
+            onValueChange={props.onChange} // his way
+            />
+        </View>
+    )
+}
 
 const FiltersScreen = props => {
+
+    const [ isGF, setIsGF ] = useState(false)
+    const [ isLacFree, setIsLacFree ] = useState(false)
+    const [ isVegan, setIsVegan ] = useState(false)
+    const [ isVegetarian, setIsVegetarian ] = useState(false)
+
     return (
         <View style={styles.screen}>
-            <Text>FILTER SCREEN</Text>
+            <Text style={styles.title}>Filter By: </Text>
+            <FilterSwitch label='Gluten-Free' value={isGF} onChange={(isGF) => setIsGF(isGF)}/>
+            <FilterSwitch label='Lactose-Free' value={isLacFree} onChange={(isLacFree) => setIsLacFree(isLacFree)}/>
+            <FilterSwitch label='Vegan' value={isVegan} onChange={(isVegan) => setIsVegan(isVegan)}/>
+            <FilterSwitch label='Vegetarian' value={isVegetarian} onChange={(isVegetarian) => setIsVegetarian(isVegetarian)}/>
         </View>
+        
     )
 }
 
@@ -29,8 +57,20 @@ FiltersScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
+        alignItems: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        justifyContent: 'center'
+        width: '80%',
+        marginVertical: 15
+    }, 
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
     }
 })
 
