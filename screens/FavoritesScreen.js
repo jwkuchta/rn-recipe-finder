6d002c695bbeb0875c27e90f482a971cfa39973f
Justina Kuchta <justyna.kuchta@gmail.com>
2020-09-02 08:52:56 -0400

    added fallback text for favorites screen if no favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,11 +4,21 @@ import MealList from '../components/MealList'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../components/HeaderButton'
 import { useSelector } from 'react-redux'
+import DefaultText from '../components/DefaultText'
+import { View, Text, StyleSheet } from 'react-native'
 
 const FavoritesScreen = props => {
     // we are here faking the data for favorite meals
 
     const favMeals = useSelector(state => state.meals.favoriteMeals)
+
+    if (favMeals.length === 0 || !favMeals) {
+        return (
+            <View style={styles.content}>
+                <DefaultText>No favorites yet</DefaultText>
+            </View>
+        )
+    }
 
     return (
         <MealList listData={favMeals} navigation={props.navigation} />
@@ -33,3 +43,11 @@ FavoritesScreen.navigationOptions = (navData) => {
 }
 
 export default FavoritesScreen
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
